c153ad46bd9e1c7ba2430448bb8cbe78a1d26da2
Ruben A. Batista <alexbatista158@hotmail.com>
2019-10-19 23:29:17 -0400

    Actualizando app con react hooks

--- src/Componests/App.js
+++ src/Componests/App.js
@@ -1,59 +1,60 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Header from './Header';
 import Form from './Form';
 import Clima from './Clima';
 import '../Styles/index.css';
-class App extends React.Component{
 
 
-  state=  {
+const App= (props)=>{
+
+  const [ClimaData, SetClima]= useState({
     Busqueda: {},
     Resultado: {},
     SearchSuccess: false
-  }
+  })
 
-  RealizarBusqueda= (Data)=>{
-      this.setState({
-        Busqueda: Data,
-      })
-      const {Ciudad, Pais}= Data;
-    if (Ciudad!== null && Pais!== null){
-      
+  const RealizarBusqueda= Data=>{
+    SetClima({
+      Busqueda: Data
+    });
+    const { Ciudad, Pais } = Data;
+    if (Ciudad !== null && Pais !== null) {
       //Realizar la consulta al API
-      const AppID= '1f0a12e742f99cb53986dcc4ba9f1aa6';
-      let url= `https://api.openweathermap.org/data/2.5/weather?q=${Ciudad},${Pais}&lang=es&appid=${AppID}`;
-      
+      const AppID = "1f0a12e742f99cb53986dcc4ba9f1aa6";
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${Ciudad},${Pais}&lang=es&appid=${AppID}`;
+
       //consultar la api
-      fetch(url).then((Response)=>{
-        return Response.json();
-      }).then(json=>{
-        this.setState({
-          Resultado: json,
-          SearchSuccess: true
+      fetch(url)
+        .then(Response => {
+          return Response.json();
         })
-      })
-      .catch(error=> console.log(error));
+        .then(json => {
+          SetClima({
+            Resultado: json,
+            SearchSuccess: true
+          });
+        })
+        .catch(error => console.log(error));
     }
   }
 
-  ShowClima= ()=>{
-      if (this.state.SearchSuccess){
-        return (
-          <Clima Datos={this.state.Resultado}></Clima>
-        )
-      }
-  }
-  render(){
-    return (
-      <div>
-        <Header Title="Consulta el Clima"></Header>
-        <Form RealizarBusqueda= {this.RealizarBusqueda}></Form>
-        <br/>
-        {this.ShowClima()}
-      </div>
-    )
+  const ShowClima= ()=>{
+    if (ClimaData.SearchSuccess){
+      return (
+        <Clima Datos={ClimaData.Resultado}></Clima>
+      )
+    }
   }
 
+  return (
+    <div>
+      <Header Title="Consulta el Clima v1.0"></Header>
+      <Form RealizarBusqueda= {RealizarBusqueda}></Form>
+      <br/>
+      {ShowClima()}
+    </div>
+  )
 }
+  
 
 export default App;
--- src/Componests/Form.js
+++ src/Componests/Form.js
@@ -1,48 +1,40 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import Alert from './AlertMessage'
 import PropTypes from 'prop-types';
 
-class Form extends Component{
+        let PaisRef = React.createRef();
+        let CiudadRef = React.createRef();
 
-    constructor(props){
-        super(props)
-        this.PaisRef= React.createRef();
-        this.CiudadRef= React.createRef();
-        this.state= {
-            HasErros: false
-        }
-    }
+const Form= (props)=>{
 
-    BuscarClima= (event)=>{
+    const [FormStatus, SetStatus]= useState({
+        HasErros: false
+    })
+
+
+    const BuscarClima= (event)=>{
         event.preventDefault();
         
         //Leer los datos para despues enviarlos por props:
-        if (this.ValidarDatos()){
+        if (ValidarDatos()){
             //Crear el objeto:
             const SearchData= {
-                Ciudad: this.CiudadRef.current.value,
-                Pais: this.PaisRef.current.value
+                Ciudad: CiudadRef.current.value,
+                Pais: PaisRef.current.value
             }
-            this.props.RealizarBusqueda(SearchData);
+            props.RealizarBusqueda(SearchData);
         }
         else{
-            this.setState({
+            SetStatus({
                 HasErros: true
             })
         }
     }
-    ValidarDatos=()=>{
-        if (this.PaisRef.current.value!== '' && this.CiudadRef.current.value!== ''){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    ShowErrorAlert= ()=>{
-        if (this.state.HasErros){
+
+    const ShowErrorAlert= ()=>{
+        if (FormStatus.HasErros){
             setTimeout(()=>{
-                this.setState({
+                SetStatus({
                     HasErros: false
                 })
             },10000)
@@ -52,39 +44,46 @@ class Form extends Component{
         }
     }
 
-
-    render(){
-        return (
-          <div className="contenedor-form">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm">
-                        <label style={{color: 'white'}}>Ciudad</label>
-                        <input className="form-control" style={{color: 'white'}} type="text" placeholder="Escribe tu ciudad" ref={this.CiudadRef}/>
-                    </div>
-                    <div className="col-sm">
-                        <label style={{color: 'white'}}>Pais</label>
-                        <select className="form-control" style={{color: 'white'}} ref={this.PaisRef}>
-                            <option value="" style={{color: 'black'}} defaultValue>--Seleccione--</option>
-                            <option value="DO" style={{color: 'black'}}>Republica Dominicana</option>
-                            <option value="MX" style={{color: 'black'}}>Mexico</option>
-                            <option value="CO" style={{color: 'black'}}>Colombia</option>
-                            <option value="US" style={{color: 'black'}}>Estados Unidos</option>
-                            <option value="PE" style={{color: 'black'}}>Peru</option>
-                        </select>
-                    </div>
-                    <div className="col-sm">
-                        <button className="btn btn-success" onClick={this.BuscarClima}><i className="fas fa-check"></i> Buscar Estado</button>
-                    </div>
-                </div>
-                <br/>
-                <br/>
-                {this.ShowErrorAlert()}
-            </div>
-          </div>
-        )
+    const ValidarDatos=()=>{
+        if (PaisRef.current.value!== '' && CiudadRef.current.value!== ''){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
+    return (
+        <div className="contenedor-form">
+          <div className="container">
+              <div className="row">
+                  <div className="col-sm">
+                      <label style={{color: 'white'}}>Ciudad</label>
+                      <input className="form-control" style={{color: 'white'}} type="text" placeholder="Escribe tu ciudad" ref={CiudadRef}/>
+                  </div>
+                  <div className="col-sm">
+                      <label style={{color: 'white'}}>Pais</label>
+                      <select className="form-control" style={{color: 'white'}} ref={PaisRef}>
+                          <option value="" style={{color: 'black'}} defaultValue>--Seleccione--</option>
+                          <option value="DO" style={{color: 'black'}}>Republica Dominicana</option>
+                          <option value="MX" style={{color: 'black'}}>Mexico</option>
+                          <option value="CO" style={{color: 'black'}}>Colombia</option>
+                          <option value="US" style={{color: 'black'}}>Estados Unidos</option>
+                          <option value="PE" style={{color: 'black'}}>Peru</option>
+                      </select>
+                  </div>
+                  <div className="col-sm">
+                      <button className="btn btn-success" onClick={BuscarClima}><i className="fas fa-check"></i> Buscar Estado</button>
+                  </div>
+              </div>
+              <br/>
+              <br/>
+              {ShowErrorAlert()}
+          </div>
+        </div>
+      )
 }
+
+
 
 
 Form.propTypes= {
